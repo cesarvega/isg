e8f02051dde0556d80e1d4cb5ed7df5c20d39bc8
Franz Bascope <franzbascope123@gmail.com>
2021-07-06 16:40:05 -0400

    refactor - move confirmation page to recap page

--- frontier/src/app/frontier/pages/confirmation/confirmation.component.ts
+++ frontier/src/app/frontier/pages/confirmation/confirmation.component.ts
@@ -1,11 +1,4 @@
-import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import { QuoteApiService } from '../../utils/services/api/quote-api.service';
-import { TasksApiService } from '../../utils/services/api/tasks-api.service.';
-import { ErrorInterface } from '../../utils/services/interfaces/common/error-interface';
-import { acceptQuoteTaskName, billPreviewTaskName } from '../../utils/taskNames';
-import { BillingApiService } from './services/billing-api.service';
 
 @Component({
   selector: 'app-confirmation',
@@ -14,64 +7,11 @@ import { BillingApiService } from './services/billing-api.service';
 })
 export class ConfirmationComponent implements OnInit {
 
-  error: ErrorInterface;
-  loading = false;
-  billPreviewHtml;
-
-  constructor(private billingApiService: BillingApiService, private sanitizer: DomSanitizer, private taskApiService: TasksApiService, private quoteApiService: QuoteApiService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.initComponent();
+
   }
 
-  async initComponent() {
-    let response = await this.makeRequest(this.getBillPreview.bind(this));
-    this.billPreviewHtml = this.sanitizer.bypassSecurityTrustHtml(response);
-  }
-
-  async placeOrder() {
-    this.loading = true;
-    try {
-      await this.taskApiService.getTasks();
-      await this.taskApiService.closeTask(billPreviewTaskName)
-      await this.taskApiService.getTasks();
-      await this.taskApiService.closeTask(acceptQuoteTaskName);
-      await this.quoteApiService.submitQuote();
-    } catch (error) {
-      this.loading = false;
-      this.error = error;
-      return;
-    } finally {
-      this.loading = false;
-    }
-  }
-
-  private getBillPreview() {
-    return this.billingApiService.getBillPreview();
-  }
-
-  private async makeRequest(request) {
-    this.beforeRequest();
-    try {
-      return await request();
-    } catch (error) {
-      this.handleError(error);
-      throw new Error(error)
-    } finally {
-      this.afterRequest();
-    }
-  }
-
-  private beforeRequest() {
-    this.loading = true;
-  }
-
-  private afterRequest() {
-    this.loading = false;
-  }
-
-  private handleError(error) {
-    this.error = error;
-  }
 
 }
--- frontier/src/app/frontier/pages/recap/recap.component.ts
+++ frontier/src/app/frontier/pages/recap/recap.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { QuoteApiService } from '../../utils/services/api/quote-api.service';
+import { TasksApiService } from '../../utils/services/api/tasks-api.service.';
+import { ErrorInterface } from '../../utils/services/interfaces/common/error-interface';
+import { acceptQuoteTaskName, billPreviewTaskName } from '../../utils/taskNames';
+import { BillingApiService } from '../confirmation/services/billing-api.service';
 
 @Component({
   selector: 'app-recap',
@@ -7,9 +13,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecapComponent implements OnInit {
 
-  constructor() { }
+  error: ErrorInterface;
+  loading = false;
+  billPreviewHtml;
+
+  constructor(private billingApiService: BillingApiService, private sanitizer: DomSanitizer, private taskApiService: TasksApiService, private quoteApiService: QuoteApiService) { }
 
   ngOnInit(): void {
+    this.initComponent();
+  }
+
+  async initComponent() {
+    let response = await this.makeRequest(this.getBillPreview.bind(this));
+    this.billPreviewHtml = this.sanitizer.bypassSecurityTrustHtml(response);
+  }
+
+  async placeOrder() {
+    this.loading = true;
+    try {
+      await this.taskApiService.getTasks();
+      await this.taskApiService.closeTask(billPreviewTaskName)
+      await this.taskApiService.getTasks();
+      await this.taskApiService.closeTask(acceptQuoteTaskName);
+      await this.quoteApiService.submitQuote();
+    } catch (error) {
+      this.loading = false;
+      this.error = error;
+      return;
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  private getBillPreview() {
+    return this.billingApiService.getBillPreview();
+  }
+
+  private async makeRequest(request) {
+    this.beforeRequest();
+    try {
+      return await request();
+    } catch (error) {
+      this.handleError(error);
+      throw new Error(error)
+    } finally {
+      this.afterRequest();
+    }
+  }
+
+  private beforeRequest() {
+    this.loading = true;
+  }
+
+  private afterRequest() {
+    this.loading = false;
+  }
+
+  private handleError(error) {
+    this.error = error;
   }
 
 }
