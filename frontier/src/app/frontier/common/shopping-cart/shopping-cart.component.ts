2b3eadd218ef30ad9be4c9febf26a997d63c092b
Franz Bascope <franzbascope123@gmail.com>
2021-07-09 17:37:21 -0400

    feat - add total due today

--- frontier/src/app/frontier/common/shopping-cart/shopping-cart.component.ts
+++ frontier/src/app/frontier/common/shopping-cart/shopping-cart.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { DepositResponse } from '../../pages/billing/payment/interfaces/deposit-requirements-response.interface';
+import { getTotalPayment } from '../../pages/billing/payment/services/deposit-request-builder.service';
 import { AccountFormInterface } from '../../utils/services/interfaces/customer/credit-check-form';
 import { OffersInterface } from '../../utils/services/interfaces/products/offers-interface';
 import { selectParsedAddress } from '../../utils/store/complexSelectors/address-parsed-selector';
-import { selectAccountForm, selectSelectedProducts } from '../../utils/store/selectors';
+import { selectAccountForm, selectDepositRequirements, selectSelectedProducts } from '../../utils/store/selectors';
 
 @Component({
   selector: 'shopping-cart',
@@ -18,11 +20,13 @@ export class ShoppingCartComponent implements OnInit {
   accountForm$: Observable<AccountFormInterface>;
   selectedAddress$: Observable<string>;
   selectedProducts$: Observable<OffersInterface[]>;
+  depositResponse$: Observable<DepositResponse>;
 
   constructor(private store: Store<any>) {
     this.accountForm$ = this.store.select(selectAccountForm);
     this.selectedAddress$ = this.store.select(selectParsedAddress);
     this.selectedProducts$ = this.store.select(selectSelectedProducts);
+    this.depositResponse$ = this.store.select(selectDepositRequirements);
   }
 
   getMonthlyTotalPrice(selectedProducts: OffersInterface[]): number {
@@ -31,6 +35,16 @@ export class ShoppingCartComponent implements OnInit {
       totalMonthly += product.bestPriceTerm.discountedPrice;
     }
     return totalMonthly;
+  }
+
+  getOneTimePrice(): number {
+    return 0;
+  }
+
+  getDueToday(depositResponse: DepositResponse): number {
+    if (!depositResponse)
+      return 0
+    return getTotalPayment(depositResponse);
   }
 
   ngOnInit(): void {
