bd8d70ca2f7269c70fae127ad2c08b018fc433ae
Franz Bascope <fb12263@infsalesgroup.com>
2021-07-15 11:45:15 -0400

    feat - add functionality to credit check questions

--- frontier/src/app/frontier/pages/credit-check/challenge-questions/challenge-questions.component.ts
+++ frontier/src/app/frontier/pages/credit-check/challenge-questions/challenge-questions.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ErrorInterface } from 'src/app/frontier/utils/services/interfaces/common/error-interface';
 import { CreditCheckResultInterface } from 'src/app/frontier/utils/services/interfaces/customer/credit-check-result';
 import { SecurityChallengeQuestionsRequestInterface } from './helpers/interfaces/securityChallengeQuestionRequest.interface';
 
@@ -11,9 +12,23 @@ export class ChallengeQuestionsComponent implements OnInit {
 
   @Input() creditCheckResult: CreditCheckResultInterface
   @Output() submitIdentityForm = new EventEmitter<SecurityChallengeQuestionsRequestInterface>();
+  error: ErrorInterface;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  submitQuestions() {
+    try {
+      for (let question of this.creditCheckResult.fraudPrevention.securityChallengeQuestions) {
+        if (!question.answerChoiceNumber) {
+          window.scroll(0, 0)
+          throw new Error("Need to answer all the questions");
+        }
+      }
+    } catch (error) {
+      this.error = error;
+    }
   }
 
 }
